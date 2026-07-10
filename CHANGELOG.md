b51ba6db8ddf7da2786631a1cf0c0478ea6802c3
Raghu Betina <raghu@firstdraft.com>
2026-07-10 14:08:32 -0500

    Support strict Content Security Policy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+- Reuse Rails' per-request Content Security Policy nonce for the console stylesheet and injected web-console assets
+
 ## [0.1.5] - 2025-09-15
 
 ### Fixed
--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class NavigationTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the console uses the response Content Security Policy nonce" do
+    get "/rails/console"
+
+    assert_response :success
+    nonce = response.headers.fetch("Content-Security-Policy")[/nonce-([^']+)/, 1]
+    assert nonce.present?
+    assert_includes response.body, %(<style nonce="#{nonce}">)
+
+    scripts = response.body.scan(/<script\b[^>]*>/)
+    assert_operator scripts.length, :>=, 3
+    scripts.each { |script| assert_includes script, %(nonce="#{nonce}") }
+    refute_includes response.body, %(nonce="")
+  end
 end
